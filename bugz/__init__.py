90b1e8818a9274f89efc71b19b367d2bff429433
Jesus Rivero <jesus@meetup.com>
2012-02-29 17:59:59 -0500

    Bumped version number

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -19,13 +19,13 @@ Classes
 
 """
 
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
 __contributors__ = ['Santiago M. Mola <cooldwind@gmail.com',
-					'William Hubbs <w.d.hubbs@gmail.com']
+					'William Hubbs <w.d.hubbs@gmail.com',
+                    'Jesus Rivero <jesus@meetup.com']
 __revision__ = '$Id: $'
 __license__ = """Copyright (c) 2006, Alastair Tse, All rights reserved.
 This following source code is licensed under the GPL v2 License."""
 
 CONFIG_FILE = '.bugz'
-
